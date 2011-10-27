eb79fc39cc15c1aff2bc77f166cb5a7ceff33fb8
inner-whisper <maks.rydkin@gmail.com>
2011-10-27 20:57:18 +0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RPD12::Application.config.secret_token = 'cb08ea303050814778f7c45416cbcc030e12cf59ac7b58b7be4f9ed1c86813861aa476480d485602d8cecc534af045e746e1ff90de4b420aa1e3be48262927f4'
