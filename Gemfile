a4361760ee8cad699f412a01d8ac3cca7a47bb3c
Max <maks.rydkin@gmail.com>
2012-01-09 14:09:31 +0400

    Some interrrrnationalization

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.1.0'
 gem 'sqlite3'
 gem 'russian', '~> 0.6.0'
 gem 'haml'
+gem 'dynamic_form'
 
 
 # Gems used only for assets and not required
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_locale
+
+  def default_url_options(options={})
+  	logger.debug "default_url_options is passed options: #{options.inspect}\n"
+  	{ :locale => I18n.locale }
+  end
+
+	protected
+		# Set the locale from parameters
+		def set_locale
+			I18n.locale = params[:locale] unless params[:locale].blank?
+		end
 end
