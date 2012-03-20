91a86f7e933375a18562f244b7d8565fe70e1526
inner-whisper <maks.rydkin@gmail.com>
2012-03-20 17:25:20 +0400

    deleted assets

--- db/migrate/20120319091449_add_translation_tables_to_post.rb
+++
@@ -1,9 +0,0 @@
-class AddTranslationTablesToPost < ActiveRecord::Migration
-  def up
-  	Post.create_translation_table! :title => :string, :body => :text
-  end
-
-  def down
-  	Post.drop_translation_table!
-  end
-end
