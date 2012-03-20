a1285ac7c365b10e56a5a91ef3c1b5c7e6d93769
inner-whisper <maks.rydkin@gmail.com>
2012-03-21 00:40:18 +0400

    Some administration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,14 +14,42 @@ class UsersController < ApplicationController
 	end
 
 	def create
-    	@user = User.new(params[:user])
+    @user = User.new(params[:user])
     	
-    	if @user.save
-    		sign_in @user
-      	redirect_to :controller => 'speeches', :action => 'new'
-    	else
-      	@title = t(:signup_page)
-      	render 'new'
-    	end
-  	end
+    if @user.save
+    	sign_in @user
+      redirect_to :controller => 'speeches', :action => 'new'
+    else
+      @title = t(:signup_page)
+      render 'new'
+    end
+  end
+ 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.toggle!(:admin)
+    if @user.update_attributes(params[:user])
+      redirect_to @user, :notice => 'Прaва пользователя были успешно обновлены.'
+    else
+      render :action => "edit"
+    end
+  end
+
+  def delete
+    @speech = User.find(params[:id])
+  end
+
+  def destroy
+    @speech = User.find(params[:id]).destroy
+    flash[:success] = "Пользователь был удален."
+    redirect_to users_path
+  end
 end
