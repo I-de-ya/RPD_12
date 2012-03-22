7d172f5246b0204d53da839ba2360f584729e296
inner-whisper <maks.rydkin@gmail.com>
2012-03-22 12:17:25 +0400

    Capistrano plus carrierwave

--- app/uploaders/thesis_uploader.rb
+++ app/uploaders/thesis_uploader.rb
@@ -27,7 +27,7 @@ class ThesisUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
-
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
   # Create different versions of your uploaded files:
   # version :thumb do
   #   process :scale => [50, 50]
@@ -35,9 +35,9 @@ class ThesisUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+     %w(doc)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
